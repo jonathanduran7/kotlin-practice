1954076efb6a77bc717ed54b9f8e04a19099ff30
jonathanduran7 <jduran@genit.com.ar>
2024-11-21 09:13:26 -0300

    add floating actions buttons

--- app/src/main/java/com/example/myapplicationcourse/firstapp/imccalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/myapplicationcourse/firstapp/imccalculator/ImcCalculatorActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplicationcourse.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
@@ -16,11 +17,19 @@ class ImcCalculatorActivity : AppCompatActivity() {
 
     private var isMaleSelected: Boolean = true
     private var isFemaleSelected: Boolean = false
+    private var currentWeight: Int = 64
+    private var currentAge: Int = 18
 
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
     private lateinit var tvHeight: TextView
     private lateinit var rsHeight: RangeSlider
+    private lateinit var btnSubtractWeight: FloatingActionButton
+    private lateinit var btnAddWeight: FloatingActionButton
+    private lateinit var tvWeight: TextView
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnAddAge: FloatingActionButton
+    private lateinit var tvAge: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +50,12 @@ class ImcCalculatorActivity : AppCompatActivity() {
         viewFemale = findViewById(R.id.viewFemale)
         tvHeight = findViewById(R.id.tvHeight)
         rsHeight = findViewById(R.id.rsHeight)
+        btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
+        btnAddWeight = findViewById(R.id.btnAddWeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        btnSubtractAge = findViewById(R.id.btnSubtractAge)
+        btnAddAge = findViewById(R.id.btnAddAge)
+        tvAge = findViewById(R.id.tvAge)
     }
 
     private fun initListeners() {
@@ -57,6 +72,28 @@ class ImcCalculatorActivity : AppCompatActivity() {
             val result = df.format(value)
             tvHeight.text = "$result cm"
         }
+
+        btnSubtractWeight.setOnClickListener {
+            currentWeight -= 1
+            setWeight()
+        }
+
+        btnAddWeight.setOnClickListener {
+            currentWeight += 1
+            setWeight()
+        }
+        btnSubtractAge.setOnClickListener {
+            currentAge -= 1
+            setAge()
+        }
+        btnAddAge.setOnClickListener {
+            currentAge += 1
+            setAge()
+        }
+    }
+
+    private fun setWeight(){
+        tvWeight.text = currentWeight.toString()
     }
 
     private fun changeGender(viewName: String){
@@ -85,7 +122,13 @@ class ImcCalculatorActivity : AppCompatActivity() {
         return ContextCompat.getColor(this, colorReference)
     }
 
+    private fun setAge(){
+        tvAge.text = currentAge.toString()
+    }
+
     private fun initUI(){
+        setAge()
+        setWeight()
         setGenderColor()
     }
 }
